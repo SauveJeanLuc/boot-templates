20cf0d2819d48e5e5429cde3d667d5526508862b
SauveJeanLuc <sauvejeanluc3@gmail.com>
2021-07-17 17:36:07 +0200

    values in objects props

--- src/index.js
+++ src/index.js
@@ -3,34 +3,47 @@ import ReactDom from 'react-dom';
 //CSS
 import './index.css';
 //variables
-const author = "SAUVÉ Jean-Luc";
-const title = "Library MIS REST";
+const firstBook = {
+  author: "SAUVÉ Jean-Luc",
+  title: "Library MIS REST",
+  img: "https://jooinn.com/images/library-37.jpg"
+}
 
+const secondBook = {
+  author: "SAUVÉ Jean-Luc",
+  title: "School MIS REST",
+  img: "https://static.toiimg.com/thumb/msid-81604332,width-1200,height-900,resizemode-4/.jpg"
+}
 function TemplateList() {
   return (
     <section className="templatelist">
-      <Template />
+      <Template author={firstBook.author} title={firstBook.title} img={firstBook.img} />
+      <Template
+        author={secondBook.author}
+        title={secondBook.title} 
+        img={secondBook.img}
+      />
     </section>
   );
 };
 
 
 
-const Template = () =>{
-
+const Template = (props) =>{
+  console.log(props);
   return (
     <article className="template">
       <img
-        src="https://jooinn.com/images/library-37.jpg"
+        src={props.img}
         width="300px"
-        alt=""
+        alt="API representation"
         className="intro__img"
       />
-      <h1>{title}</h1>
+      <h1>{props.title}</h1>
       <h4
         style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}
       >
-        {author}
+        {props.author}
       </h4>
     </article>
   );
