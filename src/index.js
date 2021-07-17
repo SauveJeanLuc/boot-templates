84d23c9ad4c1a65fc0609b275831ebda98779066
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2021-07-17 12:57:51 +0200

    html pretty set up

--- src/index.js
+++ src/index.js
@@ -4,17 +4,29 @@ import ReactDom from 'react-dom';
 function TemplateList() {
   return (
     <section>
-      <Template/>
+      <Template />
+      <Template />
+      <Template />
+      <Template />
+      <Template />
     </section>
-  )
+  );
 };
 
 const Template = () =>{
-  return <article>
-    <Image></Image>
+  return (
+  <article>
+    <Image/>
+    <Title/>
+    <Author/>
   </article>
+  );
 }
 const Image = () => (
   <img src="https://jooinn.com/images/library-37.jpg" width="300px" alt="" />
 );
+
+const Title = () => <h1>Library MIS REST</h1>
+
+const Author = () => <h4>SAUVÉ Jean-Luc</h4>;
 ReactDom.render(<TemplateList/>, document.getElementById('root'));
