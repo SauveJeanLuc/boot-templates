3bb943bed44053e14382dd8fb9e5ce214bd1a76e
SauveJeanLuc <sauvejeanluc3@gmail.com>
2021-07-17 16:25:46 +0200

    intro to jsx variables

--- src/index.js
+++ src/index.js
@@ -2,33 +2,38 @@ import React from 'react';
 import ReactDom from 'react-dom';
 //CSS
 import './index.css';
+//variables
+const author = "SAUVÉ Jean-Luc";
+const title = "Library MIS REST";
 
 function TemplateList() {
   return (
     <section className="templatelist">
       <Template />
-      <Template />
-      <Template />
-      <Template />
-      <Template />
     </section>
   );
 };
 
+
+
 const Template = () =>{
+
   return (
-  <article className="template">
-    <Image/>
-    <Title/>
-    <Author/>
-  </article>
+    <article className="template">
+      <img
+        src="https://jooinn.com/images/library-37.jpg"
+        width="300px"
+        alt=""
+        className="intro__img"
+      />
+      <h1>{title}</h1>
+      <h4
+        style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}
+      >
+        {author}
+      </h4>
+    </article>
   );
 }
-const Image = () => (
-  <img src="https://jooinn.com/images/library-37.jpg" width="300px" alt="" />
-);
 
-const Title = () => <h1>Library MIS REST</h1>
-
-const Author = () => <h4 style={{color:'#617d98', fontSize:'0.75rem', marginTop: '0.25rem' }}>SAUVÉ Jean-Luc</h4>;
 ReactDom.render(<TemplateList/>, document.getElementById('root'));
